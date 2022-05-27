c96ef6e2d77c1678b41bdd02ecc759c886ef50ca
Fırat Çelik <74001802+frt3878@users.noreply.github.com>
2022-05-27 15:09:16 +0300

    Update main.cpp

--- RFID_tag-main/main.cpp
+++ RFID_tag-main/main.cpp
@@ -1,5 +1,5 @@
 #include "mainwindow.h"
-#include<QFile>
+
 #include <QApplication>
 #include <QSerialPort>
 #include <QSerialPortInfo>
@@ -8,13 +8,6 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
-    QFile file("QTDark.stylesheet");
-     file.open(QFile::ReadOnly);
-     QString styleSheet = QLatin1String(file.readAll());
-
-     qApp->setStyleSheet(styleSheet);
-
     MainWindow w;
     w.show();
     return a.exec();
