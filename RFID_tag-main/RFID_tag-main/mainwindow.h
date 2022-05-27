ab1bbc119b902dedd2af56ca857a40278ffa027c
Fırat Çelik <74001802+frt3878@users.noreply.github.com>
2022-05-27 15:04:26 +0300

    Create mainwindow.h

---
+++ RFID_tag-main/RFID_tag-main/mainwindow.h
@@ -0,0 +1,49 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QSerialPort>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+
+    void on_add_clicked();
+
+    void on_remove_clicked();
+
+    void on_allremove_clicked();
+
+    void on_update_clicked();
+
+    void on_connect_clicked();
+
+    void Read_Data();
+    void serialWrite(QByteArray message);
+
+
+
+    void on_disconnect_clicked();
+
+    void on_send_clicked();
+
+private:
+    void serialRead();
+    void serialReadyRead();
+    uint CRC(QByteArray buf, int len);
+    Ui::MainWindow *ui;
+    QByteArray serialBuffer;
+
+
+};
+#endif // MAINWINDOW_H
