062f278f140d978db1c78ee643bc1493d5519513
Fırat Çelik <74001802+frt3878@users.noreply.github.com>
2022-05-27 16:41:23 +0300

    Update mainwindow.cpp

--- RFID_tag-main/mainwindow.cpp
+++ RFID_tag-main/mainwindow.cpp
@@ -67,18 +67,52 @@ void MainWindow::Read_Data()
 
 void MainWindow::on_add_clicked()
 {
-
+    QString addtag ="0201";
+    QByteArray bytes1 = QByteArray::fromHex(addtag.toLocal8Bit());
+    QString RFID =ui->rfiddata->toPlainText();
+    bytes1.append(QByteArray::fromHex(RFID.toLocal8Bit()));
+    uint16_t test;
+    test = CRC(bytes1,6);
+    uint8_t first[2];
+    first[0]= (0xFF)&(test>>8);
+    first[1]=(0xFF)&test;
+    bytes1.append(QByteArray::fromRawData((const char *)first,2));
+    qDebug()<<"giden data  = "<<bytes1;
+    serialWrite(bytes1);
+    qDebug()<<RFID;
 }
 
 
 void MainWindow::on_remove_clicked()
 {
-
+    QString addtag ="0202";
+    QByteArray bytes1 = QByteArray::fromHex(addtag.toLocal8Bit());
+    QString RFID =ui->rfiddata->toPlainText();
+    bytes1.append(QByteArray::fromHex(RFID.toLocal8Bit()));
+    uint16_t test;
+    test = CRC(bytes1,6);
+    uint8_t first[2];
+    first[0]= (0xFF)&(test>>8);
+    first[1]=(0xFF)&test;
+    bytes1.append(QByteArray::fromRawData((const char *)first,2));
+    qDebug()<<"giden data  = "<<bytes1;
+    serialWrite(bytes1);
+    qDebug()<<RFID;
 }
 
 
 void MainWindow::on_allremove_clicked()
 {
+    QString addtag ="0203464b7f41";
+    QByteArray bytes1 = QByteArray::fromHex(addtag.toLocal8Bit());
+    uint16_t test;
+    test = CRC(bytes1,6);
+    uint8_t first[2];
+    first[0]= (0xFF)&(test>>8);
+    first[1]=(0xFF)&test;
+    bytes1.append(QByteArray::fromRawData((const char *)first,2));
+    qDebug()<<"giden data  = "<<bytes1;
+    serialWrite(bytes1);
 
 }
 
@@ -175,24 +209,7 @@ void MainWindow::serialReadyRead()
     serialRead();
 }
 
-void MainWindow::on_send_clicked()
-{
-    QString addtag ="0201";
-    QByteArray bytes1 = QByteArray::fromHex(addtag.toLocal8Bit());
 
-
-    QString RFID =ui->rfiddata->toPlainText();
-    bytes1.append(QByteArray::fromHex(RFID.toLocal8Bit()));
-    uint16_t test;
-    test = CRC(bytes1,6);
-    uint8_t first[2];
-    first[0]= (0xFF)&(test>>8);
-    first[1]=(0xFF)&test;
-    bytes1.append(QByteArray::fromRawData((const char *)first,2));
-    qDebug()<<"giden data  = "<<bytes1;
-    serialWrite(bytes1);
-    qDebug()<<RFID;
-}
 
 uint16_t MainWindow::CRC(QByteArray buf, int len)
 {
